3091bd7c73d9d88a6aacd2426fa5f511f425ca3b
Usama ksk <163733830+Usamaksk@users.noreply.github.com>
2024-08-06 12:36:47 +0500

    initial
    initial commit

---
+++ android/app/src/main/kotlin/com/example/tic_tac_tio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tic_tac_tio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroScreen(),
+    );
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen>
+    with SingleTickerProviderStateMixin {
+  static var myNewFont = GoogleFonts.pressStart2p(
+      textStyle: TextStyle(color: Colors.black, letterSpacing: 3));
+  static var myNewFontWhite = GoogleFonts.pressStart2p(
+      textStyle: TextStyle(color: Colors.white, letterSpacing: 3));
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.white;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.grey[900],
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 120.0),
+                    child: Container(
+                      child: Text(
+                        "TIC TAC TOE",
+                        style: myNewFontWhite.copyWith(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: AvatarGlow(
+                      duration: Duration(seconds: 2),
+                      glowColor: Colors.white,
+                      repeat: true,
+                      startDelay: Duration(seconds: 1),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              style: BorderStyle.none,
+                            ),
+                            shape: BoxShape.circle),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[900],
+                          radius: 80.0,
+                          child: Image.asset(
+                            'lib/images/log.png',
+                            color: Colors.white,
+                            fit: BoxFit.scaleDown,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 80.0),
+                    child: Container(
+                      child: Text(
+                        "By Qalb-e-Momin",
+                        style: myNewFontWhite.copyWith(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 40, right: 40, bottom: 60),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.all(30),
+                        color: Colors.white,
+                        child: Center(
+                          child: Text(
+                            'PLAY GAME',
+                            style: myNewFont,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
